b56e7b6fa4458d8fddc1a3683a317e1e1c0e6c52
Amir <amigo2234xx@gmail.com>
2024-12-18 08:35:22 +0600

    Курс 1 текст

---
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -0,0 +1,11 @@
+package com.example.greetingcard.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GreetingCardTheme(content: @Composable () -> Unit) {
+    MaterialTheme {
+        content()
+    }
+}
